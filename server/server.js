a1d86881ba8b20d9b2ebb9135da296e362b447bd
Alan Fidelino <afidedev26@gmail.com>
2017-01-02 23:07:22 +1100

    Add PATCH/todos/:id routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express    = require('express');
+var _          = require('lodash');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
-var app = express();
-var port = process.env.PORT || 3000;
+var app        = express();
+var port       = process.env.PORT || 3000;
 
 // Local Imports
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo.js');
-var {User} = require('./models/user.js');
+var {Todo}     = require('./models/todo.js');
+var {User}     = require('./models/user.js');
 
 // MIDDLEWARE
 app.use(bodyParser.json());
@@ -74,6 +75,42 @@ app.delete('/todos/:id', (req, res)=>{
     res.status(404).send({});
   });
 });
+
+
+// UPDATE ROUTES
+app.patch('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // Validate the id
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  // Set up the conditions for the completed property
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed   = false;
+    body.completedAt = null;
+  }
+
+  // Use mongoose modifier variables here
+  // Body is an object that's why it's used in $set
+  // new:true is like returnOriginal:false in mongodb-update.js
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo: todo});
+  }).catch((e)=>{
+    res.status(404).send();
+  })
+
+
+});
+
 
 app.listen(port, function(){
   console.log("Listening at port:" + port);
